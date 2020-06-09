cb412cdcf5ba2873ec2082b809d5ee7b656bc000
josehoras <jose.horas@gmail.com>
2020-06-09 20:57:29 +0200

    Add (non-modular) first working version of tabular_td

---
+++ performance_mon.py
@@ -0,0 +1,164 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def update_performance(performance, number, level_moves):
+    performance['score'].append(number - 1)
+    smooth_score = np.sum(performance['score'][-100:]) / len(performance['score'][-100:])
+    performance['smooth_score'].append(smooth_score)
+    performance['moves'].append(level_moves)
+    smooth_move = np.sum(performance['moves'][-100:]) / len(performance['moves'][-100:])
+    performance['smooth_moves'].append(smooth_move)
+    return performance
+
+
+def plot_screens(screens):
+    rows = 3
+    cols = 3
+    n = rows * cols
+    fig = plt.figure(figsize=(cols * 2.25, rows * 2.25), dpi=100)
+
+    for i in range(n):
+        plt.subplot(rows, cols, i + 1)  # sets the number of feature maps to show on each row and column
+        plt.title(str(i), fontsize=10)
+        # plt.axis('off')
+        plt.xticks([])
+        plt.yticks([])
+        plt.imshow(screens[i], cmap='Greys')
+        # see_input = screens[i][screens[i]!=0]
+        # print(see_input)
+    plt.tight_layout()
+    fig.savefig('results/screens_plot.png')
+    plt.close("all")
+    # plt.show()
+
+
+def plot_performance(performance, file_base_name):
+    fig = plt.figure(figsize=(7, 7), dpi=100)
+
+    plt.subplot(2, 1, 1)
+    plt.title("total moves", fontsize=14)
+    plt.xticks(fontsize=10)
+    plt.yticks(fontsize=10)
+    plt.grid(which='major', axis='y', linestyle=':', linewidth=0.5)
+    plt.plot(performance['moves'])
+    plt.plot(performance['smooth_moves'])
+    plt.subplot(2, 1, 2)
+    plt.title("score", fontsize=14)
+    plt.xticks(fontsize=10)
+    plt.yticks(fontsize=10)
+    plt.grid(which='major', axis='y', linestyle=':', linewidth=0.5)
+    plt.plot(performance['score'])  # , color='g')
+    plt.plot(performance['smooth_score'])
+    # ax1 = fig.add_subplot(111)
+    # ax2 = ax1.twinx()
+    # # plt.plot(performance['score'])
+    # ax1.plot(performance['moves'])
+    # ax1.plot(performance['smooth_moves'])
+    # ax2.plot(performance['score'], color='g')
+    # ax2.set_ylim([0, 10])
+    plt.tight_layout()
+    fig.savefig(file_base_name + '.png')
+    plt.close("all")
+    # plt.show()
+
+
+def plot_value(value, x, y, file_name='td/value.png', alpha='', gamma='', r='', it=''):
+    def probs(matrix):
+        # print(matrix)
+        return np.exp(matrix) / np.sum(np.exp(matrix), axis=1).reshape(-1, 1)
+
+    def print_probs(values, x, y):
+        print("Probs on %i, %i: " % (x, y))
+        print("\t\t\t\t   Left   Right  Up     Down   Nothing")
+        for i in range(4):
+            print("  Going %s: \t%s" % (directions[i], values[i]))
+
+    actions = ['left', 'right', 'up', 'down', 'no action']
+    directions = ['left', 'right', 'up', 'down']
+    fig = plt.figure(figsize=(7, 7), dpi=100)
+
+    value_plane = value[:, :, x, y]
+
+    np.set_printoptions(formatter={'float': ' {: 0.2f}'.format})
+    # print_probs(probs(value_plane[0, 10]), 0, 10)
+    # print_probs(probs(value_plane[1, 10]), 1, 10)
+    # print_probs(probs(value_plane[5, 10]), 5, 10)
+    # print_probs(probs(value_plane[6, 10]), 6, 10)
+    # print_probs(probs(value_plane[7, 10]), 7, 10)
+    # print_probs(probs(value_plane[8, 10]), 8, 10)
+
+    d = 0
+    fig.suptitle("On direction: " + directions[d], fontsize=16)
+    for i in range(5):
+        plt.subplot(2, 3, i + 1)
+        plt.title("a: " + actions[i], fontsize=14)
+        plt.xticks([])
+        plt.yticks([])
+
+        plt.imshow(np.swapaxes(value_plane[:, :, d, i], 0, 1),
+                   interpolation='nearest', cmap='gist_heat')
+        plt.colorbar()
+    if alpha != '':
+        fig.text(0.7, 0.35, 'alpha = {}'.format(alpha), fontsize=16)
+        fig.text(0.7, 0.3, 'gamma = {}'.format(gamma), fontsize=16)
+        fig.text(0.7, 0.25, 'r = {}'.format(r), fontsize=16)
+        fig.text(0.7, 0.2, 'it = {}'.format(it), fontsize=16)
+    fig.tight_layout(pad=2.5, h_pad=0.25, w_pad=0.25)
+    fig.savefig(file_name)
+    plt.close("all")
+
+
+def plot_probs(value, x, y, file_name='td/value.png', alpha='', gamma='', r='', it=''):
+    directions = ['left', 'right', 'up', 'down']
+    fig = plt.figure(figsize=(7, 7), dpi=100)
+    value_plane = value[:, :, x, y]
+    probs_plane = np.exp(value_plane) / np.sum(np.exp(value_plane), axis=3).reshape(20, 20, 4, 1)
+
+    # d = random.randint(0, 3)
+    d = 0
+
+    ds = [{0: [0, 1, 4], 1: [2], 2: [3]},
+          {0: [0, 1, 4], 1: [2], 2: [3]},
+          {0: [2, 3, 4], 1: [0], 2: [1]},
+          {0: [2, 3, 4], 1: [0], 2: [1]}]
+    bctions = [['keep going', 'up', 'down'],
+               ['keep going', 'up', 'down'],
+               ['keep going', 'left', 'right'],
+               ['keep going', 'left', 'right']]
+
+    # fig.suptitle("On direction: " + directions[d], fontsize=16)
+    axs = []
+    rows = 4
+    for d in range(rows):
+        for i in range(3):
+            # axs[i] = plt.subplot(2, 3, i + 1)
+            axs.append(fig.add_subplot(rows, 3, (i + 1) + d * 3))
+
+            plt.title("a: " + bctions[d][i], fontsize=14)
+            plt.xticks([])
+            plt.yticks([])
+
+            probs_sum = np.zeros_like(np.swapaxes(probs_plane[:, :, d, i], 0, 1))
+            # print(ds[0])
+            for j in ds[d][i]:
+                probs_sum += np.swapaxes(probs_plane[:, :, d, j], 0, 1)
+
+            im = axs[-1].imshow(probs_sum, vmin=0, vmax=1, interpolation='nearest', cmap='viridis')
+            # if i==2:
+            #     cbar = fig.colorbar(im, ax=ax, shrink=0.62, format='%.1f') #,use_gridspec=True
+
+    if alpha != '' and rows < 3:
+        fig.text(0.7, 0.35, 'alpha = {}'.format(alpha), fontsize=16)
+        fig.text(0.7, 0.3, 'gamma = {}'.format(gamma), fontsize=16)
+        fig.text(0.7, 0.25, 'r = {}'.format(r), fontsize=16)
+        fig.text(0.7, 0.2, 'it = {}'.format(it), fontsize=16)
+    # fig.colorbar(cm.ScalarMappable(cmap='viridis'))
+    # fig.tight_layout(pad=2.5, h_pad=0.25, w_pad=0.25)
+    fig.tight_layout()
+    # cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
+    # fig.colorbar(im, cax=cbar_ax)
+    fig.subplots_adjust(right=1.05)
+    fig.colorbar(im, ax=axs, shrink=0.54, pad=0.02, aspect=10, format='%.1f')
+    fig.savefig(file_name)
+    plt.close("all")
---
+++ tabular_td_learning.py
@@ -0,0 +1,159 @@
+from game.snake_objects import Snake
+from game.snake_functions import *
+from performance_mon import *
+import os.path
+import pickle
+import torch
+
+
+# class TD():
+#     def __init__(self):
+#         return
+#     def update(self, old_value, new_value):
+#
+
+
+# MAIN FUNCTION
+black = 0, 0, 0
+white = 255, 255, 255
+blue = 0, 0, 255
+screen_size = np.array([400, 400])
+grid_size = np.array([20, 20])
+sq_size = screen_size // grid_size
+render = False
+np.set_printoptions(formatter={'float': '{: 0.2f}'.format})
+# Define model
+
+fix_pos = [5, 10]
+# Maybe load previous model
+file_dir = "td/"
+file_base = "v"
+file_post = "_a04_g099r20_mean_f"
+file_ext = ".td"
+file_name = file_dir + file_base + file_post + file_ext
+
+if not os.path.isdir(file_dir):
+    os.mkdir(file_dir)
+
+if os.path.isfile(file_name) == True:
+    value, alpha, gamma, r, performance, it = pickle.load(open(file_name, 'rb'), encoding='latin1')
+else:
+    value = np.full((grid_size[0], grid_size[1], grid_size[0], grid_size[1], 4, 5), 0.2)
+    alpha = 0.4
+    gamma = .99
+    r = 15
+    performance = {'score':[], 'smooth_score':[], 'moves':[], 'smooth_moves':[]}
+    it = 0
+
+# Start pygame screen
+pygame.init()
+screen = pygame.display.set_mode(screen_size)
+pygame.display.set_caption('Snake')
+if render==False: pygame.display.iconify()
+# Define fonts for screen messages
+font_size = int(sq_size[1]*1.5)
+font = pygame.font.SysFont("ubuntumono",  font_size)
+delay = 0
+
+record_dir = "/video/"
+
+print(value.shape)
+directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+d_name = ['left', 'right', 'up', 'down']
+a_name = ['left', 'right', 'up', 'down', 'no']
+
+# Main loop
+for i in range(it + 1, it + 200001):
+    # Create snake and first number
+    snake = Snake("square", screen_size, grid_size, speed=5)  # style "square" or "round"
+    number, number_grid, number_txt = generate_number(0, snake.grid, grid_size,
+                                                      font, white, set=fix_pos)
+    update_game_screen(screen, snake, number, number_grid * sq_size , number_txt, font, render=render)
+
+    level_moves = 0
+    number_moves = 0
+    while not(check_quit_event()):
+        level_moves += 1
+        number_moves += 1
+        # features = calculate_features(snake, number_grid, grid_size).to(device)
+        old_dir = [i for i in range(4) if (directions[i]==snake.head.dir).all()][0]
+        old_state = (snake.head.grid[0], snake.head.grid[1], number_grid[0], number_grid[1], old_dir)
+        probs_td = np.exp(value[old_state])/sum(np.exp(value[old_state]))
+
+        # Take action depending on prob of each option.
+        action = np.random.choice(range(5), p=probs_td)  # policy
+        action_taken = snake.update_move(action, number_grid, mode='AI')
+        # snake.update_move(pygame.key.get_pressed(), number_grid)  # Manual play
+        new_dir = [i for i in range(4) if (directions[i]==snake.head.dir).all()][0]
+        new_state = (snake.head.grid[0] + snake.head.dir[0], snake.head.grid[1] + snake.head.dir[1],
+                     number_grid[0], number_grid[1], new_dir)
+        if action_taken:
+            reward = torch.tensor([0], dtype=torch.float)
+            if snake.state == "just_ate":
+                number_moves = 0
+                reward += r
+                number, number_grid, number_txt = generate_number(number, snake.grid, grid_size,
+                                                                  font, white, set=fix_pos)
+                # print("At ", snake.head.grid[0], snake.head.grid[1], "going ", d_name[old_dir])
+                # print("value: ", value[old_state], "on ", old_state)
+                # print("probs: ", probs_td)
+                # print("action taken: %s (%i)" % (a_name[action], action))
+                # print("new state mean: ", np.mean(value[new_state]))
+
+                # TD.update_value(value[old_state][action], value[new_state])
+                value[old_state][action] += alpha * (reward + gamma * np.mean(value[new_state])
+                                                     - value[old_state][action])
+                # print("new value: ", value[old_state])
+                # print("new dir: ", d_name[new_dir], "on ", new_state)
+            if snake.state == "dead":
+                reward += -r
+                # print("At ", snake.head.grid[0], snake.head.grid[1], "going ", d_name[old_dir])
+                # print("old probs: ", probs_td, "on ", old_state)
+                # print("action taken: %s (%i)" % (a_name[action], action))
+                # update this value
+                value[old_state][action] += alpha * reward
+                # print("new probs: ", np.exp(value[old_state])/sum(np.exp(value[old_state])), "on ", old_state)
+                # print("new dir: ", d_name[new_dir], "on ", new_state)
+                break
+            if number > 50:
+                reward += 0
+                # update this value
+                value[old_state][action] += alpha * (reward + gamma*np.mean(value[new_state]) - value[old_state][action])
+                break
+            value[old_state][action] += alpha * (reward + gamma * np.mean(value[new_state])
+                                                 - value[old_state][action])
+        if number_moves == 1000:
+            reward += 0
+            # update this value
+            # print("\nAt ", snake.head.grid[0], snake.head.grid[1], "going ", d_name[old_dir])
+            # print("old probs: ", probs_td, "on ", old_state)
+            # print("action taken: %s (%i)" % (a_name[action], action))
+            # value[old_state][action] += alpha * (reward + gamma * np.mean(value[new_state])
+            #                                      - value[old_state][action])
+            # print("new probs: ", np.exp(value[old_state]) / sum(np.exp(value[old_state])), "on ", old_state)
+            break
+
+        update_game_screen(screen, snake, number, number_grid * sq_size , number_txt, font, render=render)
+        delay = check_delay(delay, pygame.key.get_pressed())
+
+        pygame.time.delay(delay)
+
+    performance = update_performance(performance, number, level_moves)
+    print(i, " - score: %i (%.1f), moves: %i (%.0f)" %
+          (performance['score'][-1], performance['smooth_score'][-1],
+           performance['moves'][-1], performance['smooth_moves'][-1]))
+    print()
+    if i % 50 == 0:
+        plot_performance(performance, file_dir + file_base + file_post)
+        # plot_value(value, 5, 10, file_name=file_dir + 'v_map' + file_post + '.png',
+        #            alpha=alpha, gamma=gamma, r=r, it=i)
+        plot_probs(value, fix_pos[0], fix_pos[1], file_name=file_dir + 'p_map' + file_post + '.png',
+                   alpha=alpha, gamma=gamma, r=r, it=i)
+        pickle.dump([value, alpha, gamma, r, performance, i], open(file_dir + file_base + file_post + file_ext, 'wb'))
+
+
+plot_msg("Press Esc. to quit", screen, font)
+pygame.display.update()
+while not(check_quit_event()):
+    pass
+pygame.display.quit()
