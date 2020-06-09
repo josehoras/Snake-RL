bc0a45c28bc2d3ac18670f0304896f96bc7a4157
josehoras <jose.horas@gmail.com>
2020-06-09 22:41:03 +0200

    Clean up

--- tabular_td_learning.py
+++ tabular_td_learning.py
@@ -3,7 +3,6 @@ from game.snake_functions import *
 from performance_mon import *
 import os.path
 import pickle
-import torch
 
 
 class TabTD:
@@ -27,6 +26,11 @@ class TabTD:
             self.q[s][a] += self.alpha * r
 
 
+def dir2i(dir):
+    directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+    return [i for i in range(4) if (directions[i] == dir).all()][0]
+
+
 def debug_msg_1(old_state, probs_td, action, new_state):
     d_name = ['left', 'right', 'up', 'down']
     a_name = ['left', 'right', 'up', 'down', 'no']
@@ -37,10 +41,10 @@ def debug_msg_1(old_state, probs_td, action, new_state):
     # print("new state mean: ", np.mean(model.q[new_state]))
 
 
-def debug_msg_2(old_state, new_dir):
+def debug_msg_2(old_state, new_state):
     d_name = ['left', 'right', 'up', 'down']
     print("new value: ", model.q[old_state])
-    print("new dir: ", d_name[new_dir], "on ", new_state)
+    print("new dir: ", d_name[new_state[-1]], "on ", new_state)
 
 
 # MAIN FUNCTION
@@ -82,17 +86,12 @@ font_size = int(sq_size[1]*1.5)
 font = pygame.font.SysFont("ubuntumono",  font_size)
 delay = 0
 
-record_dir = "/video/"
-
-print(model.q.shape)
-directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
 
 # Main loop
 for i in range(len(model.performance['moves']) + 1, len(model.performance['moves']) + 200001):
     # Create snake and first number
     snake = Snake("square", screen_size, grid_size, speed=5)  # style "square" or "round"
-    number, number_grid, number_txt = generate_number(0, snake.grid, grid_size,
-                                                      font, white, set=fix_pos)
+    number, number_grid, number_txt = generate_number(0, snake.grid, grid_size, font, white, set=fix_pos)
     update_game_screen(screen, snake, number, number_grid * sq_size , number_txt, font, render=render)
 
     level_moves = 0
@@ -101,40 +100,32 @@ for i in range(len(model.performance['moves']) + 1, len(model.performance['moves
         level_moves += 1
         number_moves += 1
         # features = calculate_features(snake, number_grid, grid_size).to(device)
-        old_dir = [i for i in range(4) if (directions[i]==snake.head.dir).all()][0]
-        old_state = (snake.head.grid[0], snake.head.grid[1], number_grid[0], number_grid[1], old_dir)
+        old_state = (snake.head.grid[0], snake.head.grid[1], number_grid[0], number_grid[1], dir2i(snake.head.dir))
         probs_td = np.exp(model.q[old_state])/sum(np.exp(model.q[old_state]))
-
         # Take action depending on prob of each option.
         action = np.random.choice(range(5), p=probs_td)  # policy
         action_taken = snake.update_move(action, number_grid, mode='AI')
         # snake.update_move(pygame.key.get_pressed(), number_grid)  # Manual play
-        new_dir = [i for i in range(4) if (directions[i]==snake.head.dir).all()][0]
         new_state = (snake.head.grid[0] + snake.head.dir[0], snake.head.grid[1] + snake.head.dir[1],
-                     number_grid[0], number_grid[1], new_dir)
+                     number_grid[0], number_grid[1], dir2i(snake.head.dir))
         if action_taken:
-            reward = torch.tensor([0], dtype=torch.float)
-            if snake.state == "just_ate":
-                number_moves = 0
-                reward += model.r
-                number, number_grid, number_txt = generate_number(number, snake.grid, grid_size,
-                                                                  font, white, set=fix_pos)
-                model.update_q(reward, old_state, action, new_state)
+            reward = 0
             if snake.state == "dead":
                 reward += -model.r
                 # debug_msg_1(old_state, probs_td, action, new_state)
                 model.update_q(reward, old_state, action, new_state)
-                # debug_msg_2(old_state, new_dir)
+                # debug_msg_2(old_state, new_state)
                 break
+            if snake.state == "just_ate":
+                number_moves = 0
+                reward += model.r
+                number, number_grid, number_txt = generate_number(number, snake.grid, grid_size, font, white, set=fix_pos)
             model.update_q(reward, old_state, action, new_state)
         if number_moves == 1000 or number > 50:
             break
-
         update_game_screen(screen, snake, number, number_grid * sq_size , number_txt, font, render=render)
         delay = check_delay(delay, pygame.key.get_pressed())
-
         pygame.time.delay(delay)
-
     model.performance = update_performance(model.performance, number, level_moves)
     print(i, " - score: %i (%.1f), moves: %i (%.0f)" %
           (model.performance['score'][-1], model.performance['smooth_score'][-1],
@@ -142,12 +133,11 @@ for i in range(len(model.performance['moves']) + 1, len(model.performance['moves
     print()
     if i % 50 == 0:
         plot_performance(model.performance, file_dir + file_base + file_post)
-        # plot_value(value, 5, 10, file_name=file_dir + 'v_map' + file_post + '.png',
+        # plot_value(model.q, 5, 10, file_name=file_dir + 'v_map' + file_post + '.png',
         #            alpha=alpha, gamma=gamma, r=r, it=i)
         plot_probs(model.q, fix_pos[0], fix_pos[1], file_name=file_dir + 'p_map' + file_post + '.png',
                    alpha=model.alpha, gamma=model.gamma, r=model.r, it=i)
         model.save(file_name)
-
 
 plot_msg("Press Esc. to quit", screen, font)
 pygame.display.update()
