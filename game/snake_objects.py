73b0fbe79d6df09f6bf75a06336bde40156e22bd
josehoras <jose.horas@gmail.com>
2020-06-14 18:32:17 +0200

    Refactor; add body as sprite.Group within standard class

--- game/snake_objects.py
+++ game/snake_objects.py
@@ -10,6 +10,7 @@ class SnakePart(pygame.sprite.Sprite):
     def __init__(self, grid, dir, style, size):
         super(SnakePart, self).__init__()
         self.dir = dir
+        self.grid = grid
         self.size = size
         self.body = pygame.Surface(size)
         self.rect = self.body.get_rect()
@@ -18,11 +19,7 @@ class SnakePart(pygame.sprite.Sprite):
             pygame.draw.rect(self.body, black, self.rect, 1)
         if style == "round":
             pygame.draw.circle(self.body, (0, 255, 0), size//2 , min(size)//2)
-        # pygame.draw.rect(self.body, blue, self.rect, 0)
-        # pygame.draw.line(self.body, white, (0,0), abs(sq_size * self.dir))
-        # pygame.draw.line(self.body, white, (sq_size - (1,1)) * abs(self.dir[::-1]), sq_size - (1,1) )
 
-        self.grid = grid
         self.pos = self.grid2pos()
         self.rect.topleft = self.pos
 
@@ -47,12 +44,12 @@ class SnakePart(pygame.sprite.Sprite):
     # def __eq__(self, other):
     #     pass
 
-class Snake(pygame.sprite.Group):
+class Snake():
     def __init__(self, style, screen_size, grid_size, speed=0.05):
-        super(Snake, self).__init__()
+        # super(Snake, self).__init__()
         self.grid = grid_size//2 - [[2, 0], [1, 0], [0, 0]]
         self.direction = np.array([[1, 0], [1, 0], [1, 0]])
-
+        self.body = pygame.sprite.Group()
         self.screen_size = screen_size
         self.sq_size = screen_size // grid_size
         self.style = style
@@ -64,7 +61,7 @@ class Snake(pygame.sprite.Group):
 
         self.tail = SnakePart(self.grid[0], self.direction[0], self.style, self.sq_size)
         for i in range(1, len(self.grid)-1):
-            self.add(SnakePart(self.grid[i], self.direction[i], self.style, self.sq_size))
+            self.body.add(SnakePart(self.grid[i], self.direction[i], self.style, self.sq_size))
         self.head = SnakePart(self.grid[-1], self.direction[-1], self.style, self.sq_size)
 
     def new_square(self, part):
@@ -73,7 +70,7 @@ class Snake(pygame.sprite.Group):
 
     def step(self):
         self.head.update(self.speed)
-        if len(self) >= self.length:
+        if len(self.body) >= self.length:
             self.tail.update(self.speed)
 
     def out_of_screen(self):
@@ -97,25 +94,29 @@ class Snake(pygame.sprite.Group):
             action_taken = True
             # apply new direction to head and add new body part
             self.head.dir = self.next_dir
-            self.add(SnakePart(self.head.grid, self.head.dir, self.style, self.sq_size))
+            self.body.add(SnakePart(self.head.grid, self.head.dir, self.style, self.sq_size))
             self.grid = np.append(self.grid, [self.head.grid], axis=0)
             # Move
             self.step()
             # Check dying conditions for new position
             if self.out_of_screen():
                 self.state = "dead"
-            # if len(pygame.sprite.spritecollide(self.head, self, False)) > 1:
-            #     self.state = "dead"
+            if len(pygame.sprite.spritecollide(self.head, self.body, False)) > 1:
+                self.state = "dead"
             if ((self.head.grid + self.head.dir) == number_pos).all():
                 self.length += self.length_increase
                 self.state = "just_ate"
         # Check if tail has reached a new grid square and remove that part of the body
         if self.new_square(self.tail):
-            to_del = [sp for sp in self if sp.rect.colliderect(self.tail.rect)][0]
+            to_del = [sp for sp in self.body if sp.rect.colliderect(self.tail.rect)][0]
             self.grid = self.grid[[(g != self.tail.grid).any() for g in self.grid]]
+            # self.grid = [p.grid for p in self]
+            print(self.grid)
+            print([p.grid for p in self.body])
             self.tail.dir = to_del.dir
             self.tail.grid = to_del.grid
-            self.remove(to_del)
+            self.body.remove(to_del)
+            print([p.grid for p in self.body])
         return action_taken
 
 
@@ -144,6 +145,7 @@ class Snake(pygame.sprite.Group):
             self.tail.dir = to_del.dir
             self.tail.grid = to_del.grid
             self.remove(to_del)
+
         # Check dying conditions
         if self.head.rect.top < 0 or self.head.rect.bottom > self.screen_size[1]:
             self.state = "dead"
@@ -176,5 +178,5 @@ class Snake(pygame.sprite.Group):
     def plot(self, screen):
         screen.blit(self.head.body, self.head.rect)
         screen.blit(self.tail.body, self.tail.pos)
-        for sp in self:
+        for sp in self.body:
             screen.blit(sp.body, sp.pos)
