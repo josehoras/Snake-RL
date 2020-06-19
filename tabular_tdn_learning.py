947f0d0bae13025d9c55c90c304346e24693731c
josehoras <jose.horas@gmail.com>
2020-06-19 22:19:27 +0200

    Add main() to integrate with unittest

--- tabular_tdn_learning.py
+++ tabular_tdn_learning.py
@@ -81,12 +81,12 @@ class TabTD_n:
         G = 0
         for i in range(tau + 1, min(tau + self.n, T) + 1):
             G += self.gamma ** (i - tau - 1) * R[i]
-            # print(i,': ', self.gamma**(i-tau-1), '  R: ', R[i])
+            print(i,': ', self.gamma**(i-tau-1), '  R: ', R[i])
         if not self.terminal(S[tau + self.n]): #tau + N < T+1:
-            # print(tau + N, ': ', self.gamma**N)
+            print(tau + self.n, ': ', self.gamma ** self.n)
             G += self.gamma ** self.n * self.update_rule(S[tau + self.n])
-        # print('G: %.2f' % G)
-        # print("Inc.: %.2f at state %s, action %i" % (self.alpha * (G - self.q[S[tau]][A[tau]]), S[tau], A[tau] ))
+        print('G: %.2f' % G)
+        print("Inc.: %.2f at state %s, action %i" % (self.alpha * (G - self.q[S[tau]][A[tau]]), S[tau], A[tau]))
         self.q[S[tau]][A[tau]] += self.alpha * (G - self.q[S[tau]][A[tau]])
 
     def double_q_learning(self, s, a, r, s_p):
@@ -103,7 +103,7 @@ class TabTD_n:
                 self.q2[s][a] += self.alpha * r
 
 
-def debug_msg(before=True, rew=0):
+def debug_msg(before=True, rew=0, a=False, s=''):
     if before:
         print('_'*80)
         print("Before move")
@@ -118,83 +118,87 @@ def debug_msg(before=True, rew=0):
 
 
 # MAIN FUNCTION
-screen_size = [400, 400]
-grid_size = [20, 20]
-fix_pos = [5, 10]
-env = GameSession(screen_size, grid_size, fix_number='', delay=0, render=False)
+def main():
+    screen_size = [400, 400]
+    grid_size = [20, 20]
+    fix_pos = [12, 10]
+    env = GameSession(screen_size, grid_size, fix_number=fix_pos, delay=0, render=True)
 
-# Model name
-file_dir = "results/"
-file_base = "sarsa"
-file_post = "_a04g09e10_full"
-file_ext = ".tdn"
-file_name = file_dir + file_base + file_post + file_ext
-restart = True
-batch = 1000
-# Maybe load previous model
-if os.path.isfile(file_name) and not restart:
-    model = TabTD_n(grid_size)
-    model.load(file_name)
-else:
-    if not os.path.isdir(file_dir):
-        os.mkdir(file_dir)
-    model = TabTD_n(grid_size, alpha=0.4, gamma=0.8, n=10, policy='e-greedy', update_rule='sarsa')
+    # Model name
+    file_dir = "results/"
+    file_base = "sarsa"
+    file_post = "_a04g08e1000n10"
+    file_ext = ".td"
+    file_name = file_dir + file_base + file_post + file_ext
+    restart = True
+    batch = 10
+    # Maybe load previous model
+    if os.path.isfile(file_name) and not restart:
+        model = TabTD_n(grid_size)
+        model.load(file_name)
+    else:
+        if not os.path.isdir(file_dir):
+            os.mkdir(file_dir)
+        model = TabTD_n(grid_size, alpha=0.4, gamma=0.8, n=4, policy='e-greedy', update_rule='sarsa')
 
 
-# Main loop
-for i in range(len(model.performance['moves']) + 1, len(model.performance['moves']) + 200001):
-    _, _, alive = env.start_game()
-    if i % batch == 0:
-        model.it_plus()
-    level_moves = 0
-    number_moves = 0
-    score = 1
-    T = 1
-    S = [env.get_state()]
-    A = []
-    R = [0]
+    # Main loop
+    for i in range(len(model.performance['moves']) + 1, len(model.performance['moves']) + 200001):
+        _, _, alive = env.start_game()
+        if i % batch == 0:
+            model.it_plus()
+        level_moves = 0
+        number_moves = 0
+        score = 1
+        T = 1
+        S = [env.get_state()]
+        A = []
+        R = [0]
+        t = 0
+        tau = t - model.n + 1
+        while tau < (T-1):
+            action_taken = True
+            if t < T:
+                # Take action depending on policy
+                old_state = env.get_state()
+                action = model.policy(old_state)
+                action_taken, reward, new_state, alive = env.step(action, mode='AI')
+            if action_taken:
+                level_moves += 1
+                number_moves += 1
+                # debug_msg(before=True, s=old_state)
+                # debug_msg(before=False, rew=reward, a=action_taken, s=new_state)
+                S.append(new_state)
+                A.append(action)
+                R.append(reward)
+                print('S: ', S, '\nA: ', A, '\nR: ', R)
+                tau = t - model.n + 1
+                if tau >= 0:
+                    model.update_q(S, A, R, tau, T)
+                if env.number['n'] > score:
+                    score = env.number['n']
+                    number_moves = 0
+                t += 1
+                if alive and env.number['n'] <= 50 and number_moves < 500:
+                    T += 1
+                while not env.check_continue_event():
+                    pass
 
-    t = 0
-    tau = t - model.n + 1
-    while tau < (T-1):
-        action_taken = True
-        if t < T:
-            # Take action depending on policy
-            old_state = env.get_state()
-            action = model.policy(old_state)
-            action_taken, reward, new_state, alive = env.step(action, mode='AI')
-        if action_taken:
-            level_moves += 1
-            number_moves += 1
-            # debug_msg(before=True, s=old_state)
-            # debug_msg(before=False, rew=reward, a=action_taken, s=new_state)
-            S.append(new_state)
-            A.append(action)
-            R.append(reward)
-            tau = t - model.n + 1
-            if tau >= 0:
-                model.update_q(S, A, R, tau, T)
-            if env.number['n'] > score:
-                score = env.number['n']
-                number_moves = 0
-            t += 1
-            if alive and env.number['n'] <= 50 and number_moves < 250:
-                T += 1
-            # while not env.check_continue_event():
-            #     pass
+        # Book-keeping
+        # print("%06.3f, %06.3f" % (np.min(model.q[:, :, 5, 10]), np.max(model.q[:, :, 5, 10])))
+        model.performance = update_performance(model.performance, env.number['n'], level_moves)
+        print(i, " - score: %i (%.1f), moves: %i (%.0f)" %
+              (model.performance['score'][-1], model.performance['smooth_score'][-1],
+               model.performance['moves'][-1], model.performance['smooth_moves'][-1]))
+        print()
+        if i % 1 == 0:
+            plot_performance(model.performance, file_dir + file_base + file_post)
+            # plot_value(model.q, 5, 10, file_name=file_dir + 'v_map' + file_post + '.png',
+            #            alpha=alpha, gamma=gamma, r=r, it=i)
+            plot_probs(model, fix_pos[0], fix_pos[1], file_name=file_dir + file_base + file_post)
+            model.save(file_name)
 
-    # Book-keeping
-    # print("%06.3f, %06.3f" % (np.min(model.q[:, :, 5, 10]), np.max(model.q[:, :, 5, 10])))
-    model.performance = update_performance(model.performance, env.number['n'], level_moves)
-    print(i, " - score: %i (%.1f), moves: %i (%.0f)" %
-          (model.performance['score'][-1], model.performance['smooth_score'][-1],
-           model.performance['moves'][-1], model.performance['smooth_moves'][-1]))
-    print()
-    if i % 500 == 0:
-        plot_performance(model.performance, file_dir + file_base + file_post)
-        # plot_value(model.q, 5, 10, file_name=file_dir + 'v_map' + file_post + '.png',
-        #            alpha=alpha, gamma=gamma, r=r, it=i)
-        plot_probs(model, fix_pos[0], fix_pos[1], file_name=file_dir + file_base + file_post)
-        model.save(file_name)
+    env.exit()
 
-env.exit()
+if __name__ == "__main__":
+    main()
