fbb3f04d52680f4e348dae249c48328355b04d0f
josehoras <jose.horas@gmail.com>
2020-06-20 13:24:08 +0200

    Add first unit test for tabular TD(n)

---
+++ tabular_tdn_unittest.py
@@ -0,0 +1,47 @@
+import unittest
+from tabular_tdn_learning import TabularTDn
+
+
+class TestTabularTDn(unittest.TestCase):
+    def setUp(self):
+        grid_size = [20, 20]
+        self.model = TabularTDn(grid_size, alpha=0.5, gamma=0.8, n=3, policy='e-greedy', update_rule='sarsa')
+        currentTest = self.id().split('.')[-1]
+        if currentTest == 'test_2':
+            self.skipTest('bla')
+
+    def debug_msg(self, before=True, rew=0, a=False, s=''):
+        if before:
+            print('_'*80)
+            print("Before move")
+        else:
+            print("After movee")
+        print("   State:  ",  s, self.env.snake.head.rect.topleft)
+        # print("   On grid: ", env.snake.head.on_grid())
+        if not before:
+            # if a: print("   Action taken")
+            print("   Reward: ", rew)
+            print('_' * 80)
+
+    def test_1(self):
+        # This test shows that the reward present at state S[3] gets accounted into
+        # the value of Q[S[0]][A[0]], as n=3, at timestamp 3 (t = 0, 1, 2, ...)
+        # The expected result is the reward discounted by gamma^2 and alpha
+        # At this test all Q = 0. As the value of the next state is not relevent (0)
+        # all algortithms work with this test (sarsa, expected sarsa, q-learning,...)
+        S = [(10, 10, 13, 10, 1), (11, 10, 13, 10, 1), (12, 10, 13, 10, 1), (13, 10, 13, 10, 1)]
+        A = [0, 0, 0]
+        R = [0, 0, 0, 1]
+        t = 2
+        T = 3
+        tau = t - self.model.n + 1
+        if tau >= 0:
+            self.model.update_q(S, A, R, tau, T)
+        self.assertEqual(self.model.q[S[tau]][A[tau]], self.model.alpha * self.model.gamma ** 2 * R[3])
+
+    def test_2(self):
+        print('Este test funciona :)')
+
+if __name__ == "__main__":
+    unittest.main()
+    # test()
